7ca82d1824286478e83c9c6d40d992bce9a24143
Jann1k-h <wi24b131@technikum-wien.at>
2026-06-29 14:13:47 +0200

    Grundstruktur

--- Gui/src/main/java/at/technikum/gui/Launcher.java
+++
@@ -1,9 +0,0 @@
-package at.technikum.gui;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(GuiApplication.class, args);
-    }
-}
