55cf38f71bc873d87088522139060121df221c53
Jann1k-h <wi24b131@technikum-wien.at>
2026-06-29 14:11:06 +0200

    Add RestApi and Gui

---
+++ Gui/src/main/java/at/technikum/gui/Launcher.java
@@ -0,0 +1,9 @@
+package at.technikum.gui;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(GuiApplication.class, args);
+    }
+}
